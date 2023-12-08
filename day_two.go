0cc118f756c0645ddab4d3046c688457b23f4658
tangerine <tangerinefr0g@hotmail.com>
2023-12-08 20:01:45 +0400

    Day 2 Part 2

--- day_two.go
+++ day_two.go
@@ -35,6 +35,20 @@ func DayTwoPartOne() {
 	fmt.Printf("Day 2 - Part 1 result: %d\n", sum)
 }
 
+func DayTwoPartTwo() {
+	var filename = "input_day_two"
+
+	sum := 0
+	lines := GetFileLines(filename)
+	for i := 0; i < len(lines); i++ {
+		gameSplit := strings.Split(lines[i], ": ")
+		cubeGroups := strings.Split(gameSplit[1], "; ")
+		sum += GetGamePowers(cubeGroups)
+	}
+
+	fmt.Printf("Day 2 - Part 2 result: %d\n", sum)
+}
+
 func IsGroupImpossible(group string) bool {
 	draws := strings.Split(group, ", ")
 	for i := 0; i < len(draws); i++ {
@@ -47,4 +61,33 @@ func IsGroupImpossible(group string) bool {
 		}
 	}
 	return false
+}
+
+func GetGamePowers(groups []string) int {	
+	restrictionsCopy := make(map[string]int)
+	for key := range restrictions {
+		restrictionsCopy[key] = 0
+	}
+
+	for i := 0; i < len(groups); i++ {
+		draws := strings.Split(groups[i], ", ")
+		for i := 0; i < len(draws); i++ {
+			count_color := strings.Split(draws[i], " ")
+			count, _ := strconv.Atoi(count_color[0])
+			color := count_color[1]
+
+			if restrictionsCopy[color] < count {
+				restrictionsCopy[color] = count
+			}
+		}
+	}
+
+	colorPowers := 1
+
+	for _, value := range restrictionsCopy {
+
+		colorPowers *= value
+	}
+
+	return colorPowers
 }
--- main.go
+++ main.go
@@ -5,4 +5,5 @@ func main() {
 	DayOnePartTwo()
 
 	DayTwoPartOne()
+	DayTwoPartTwo()
 }
